2459ff90450ab8a9dfd53e388f629a1921b9842a
ismith1024 <ismith1024@gmail.com>
2018-01-14 08:57:06 -0500

    Regression for PE

--- regression.r
+++ regression.r
@@ -29,5 +29,34 @@ divreg <- function(symbol, todaysPrice, todaysYield){
   print("Predicted annualized yeld: ")  
   predict(dv.mod1, data.frame(Yld = c(todaysYield)))
   
+}
 
+pereg <- function(symbol, todaysPrice, todaysPE){
+  thisYear <- 2018
+  thisMonth <- 1
+  
+  sqlString <- paste("SELECT * FROM Integrated WHERE Symbol = '", symbol,"' AND PE IS NOT NULL", sep = "")# AND pe < 10", sep = "");
+  peTable <- dbGetQuery(db, sqlString);
+  
+  peTable$CADPrice <- peTable$Xchange * peTable$Price
+  peTable$Elapsed <- thisYear - peTable$Y - (peTable$M - thisMonth)/12
+  peTable$Return <- todaysPrice / peTable$CADPrice
+  peTable$Annualized <- peTable$Return ^ (1/peTable$Elapsed)
+  
+  #print(peTable)
+  
+  pe.mod1 <- lm(Annualized ~ PE, data = peTable)
+  
+  #summary(dv.mod1)
+  plot(peTable$PE, peTable$Annualized, main = paste("P-E Regression: ", symbol, sep = ""), xlab = "P-E Ratio", ylab = "Annualized Return")
+  
+  abline(pe.mod1)
+  
+  print("Predicted annualized yeld: ")
+  td <- data.frame(PE = c(todaysPE))
+  
+  #print(td)
+  
+  predict(pe.mod1, data.frame(PE = c(todaysPE)))
+  
 }
