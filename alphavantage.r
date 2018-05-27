2f6f21fff98c03c9eaeb06e7118e7d7db48fae0e
ismith1024 <ismith1024@gmail.com>
2018-05-27 09:35:02 -0400

    alphavantage api

---
+++ alphavantage.r
@@ -0,0 +1,9 @@
+# Obtain time series price data from ALphavantage using the API
+# key is:
+# CTECN021MT4UQAJ2
+# https://www.alphavantage.co/documentation/
+# example query - full range
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BNS&&outputsize=full&apikey=CTECN021MT4UQAJ2
+# example query - default range
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BNS&apikey=CTECN021MT4UQAJ2
+
