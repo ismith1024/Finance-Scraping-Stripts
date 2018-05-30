cd9146ee467892042e9e8b1fb45884a9a68ff0c7
ismith1024 <ismith1024@gmail.com>
2018-05-30 19:17:26 -0400

    Weierstrass transform

--- alphavantage.r
+++ alphavantage.r
@@ -17,7 +17,7 @@ library(jsonlite)
 
 #obtains the USD daily time series data for the symbol
 scrapeAVdata <- function(sym){
-  theURL <- paste("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=",sym,"&apikey=CTECN021MT4UQAJ2",sep = "")
+  theURL <- paste("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=",sym,"&&outputsize=full&apikey=CTECN021MT4UQAJ2",sep = "")
   jsonText <- fromJSON(theURL, simplifyDataFrame = TRUE)
   #the dates from the jsonText DataFrame
   frames <- jsonText[[2]]
