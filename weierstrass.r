b86185a3e77f31a77ddedf543a5176b5507ccc42
ismith1024 <ismith1024@gmail.com>
2018-06-03 13:42:05 -0400

    annualized return

--- weierstrass.r
+++ weierstrass.r
@@ -2,16 +2,34 @@ library(sqldf)
 require(stats)
 require(lattice)
 
-
-db <- dbConnect(SQLite(), dbname="~/Data/tsx.db")
+db <- dbConnect(SQLite(), dbname="~/Data/CADFinance.db")
 
 #get the time series
-rs <- dbSendQuery(db, "SELECT close FROM XTSE WHERE symbol = 'BNS';") #time series
-while (!dbHasCompleted(rs)) {
-  values <- dbFetch(rs)
+getTimeSeries <- function(symb){
+  sqlQuery <- paste("SELECT close, day_, month_, year_ FROM xtse WHERE symbol = '", symb, "';", sep = "")
+  rs <- dbSendQuery(db, sqlQuery) #time series
+  while (!dbHasCompleted(rs)) {
+    values <- dbFetch(rs)
+  }
+  
+  timeSeries <- values
+  return(timeSeries)
 }
 
-timeSeries <- values[[1]]
+# provides the annualized return of the equity to the most recent date on record
+# (today's value / initial value)^(1/time); time in years
+# ser is the time series of values
+getAnnualizedReturn <- function(initDay, initMonth, initYear, initVal, finalDay, finalMonth, finalYear, finalVal){
+  years <- finalYear - initYear
+  years <- years + (finalMonth - initMonth) / 12
+  years <- years + (finalDay - initDay) / 252
+  
+  change <- (finalVal/initVal)^(1/years) 
+  return(change)
+}
+
+
+getTimeSeries("BNS")
 
 plot(timeSeries, pch = ".")
 
@@ -20,5 +38,6 @@ plot(timeSeries, pch = ".")
 kern = c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
 wTrans = convolve(timeSeries, kern, type = "filter")
 plot(wTrans, pch = ".")
+
 
 
