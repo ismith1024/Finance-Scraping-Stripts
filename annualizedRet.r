f1cdd3fdbbaba780e58c4d465b25ce96a01d6692
ismith1024 <ismith1024@gmail.com>
2018-06-03 20:06:59 -0400

    earnings plot

--- annualizedRet.r
+++ annualizedRet.r
@@ -17,6 +17,54 @@ getTimeSeries <- function(symb){
   return(timeSeries)
 }
 
+
+#retrieve a [eps, div, year, month] dataframe
+#for the given symbol
+#NOTE: earnings uses a '.' instead of a '_' character, need to substitute
+getEarnings <- function(symb){
+  theSym <- gsub("_", ".", symb)
+  sqlQuery <- paste("SELECT eps, div, year_, month_ FROM earnings WHERE symbol = '", theSym, "' ORDER BY year_, month_ ASC;", sep = "")
+  rs <- dbSendQuery(db, sqlQuery)
+  while (!dbHasCompleted(rs)) {
+    values <- dbFetch(rs)
+  }
+  
+  earn<-values
+  return(earn)
+}
+
+#get the last four quarters of EPS values from the date
+last4eps <- function(symb, year, month, day, earns){
+  i <- 1
+  found <- FALSE
+  while(found == FALSE){
+    if(earns[i, "year_"] == year && earns[i, "month_"] == month){
+      found <- TRUE
+    }
+    i = i + 1
+  }
+  if(i <= 3) return(0)
+  
+  val = earns[i, "eps"] + earns[i-1, "eps"] + earns[i-2, "eps"] + earns[i-3, "eps"] 
+  return(val)
+}
+
+#get the last four quarters of dividend values from the date
+last4divs <- function(symb, year, month, day, earns){
+  i <- 1
+  found <- FALSE
+  while(found == FALSE){
+    if(earns[i, "year_"] == year && earns[i, "month_"] == month){
+      found <- TRUE
+    }
+    i = i + 1
+  }
+  if(i <= 3) return(0)
+  
+  val = earns[i, "div"] + earns[i-1, "div"] + earns[i-2, "div"] + earns[i-3, "div"] 
+  return(val)
+}
+
 # provides the annualized return of the equity to the most recent date on record
 # (today's value / initial value)^(1/time); time in years
 # ser is the time series of values
@@ -34,7 +82,7 @@ getAnnualizedReturn <- function(initDay, initMonth, initYear, initVal, finalDay,
 #Kernel prameters:
 #n = 51
 #sigma = 10
-run <- function(symb){
+runRets <- function(symb){
   timeSer <- getTimeSeries(symb)
   #generate the kernel from here for now:
   #http://dev.theomader.com/gaussian-kernel-calculator/
@@ -66,6 +114,14 @@ run <- function(symb){
   
 }
 
+runTS <- function(symb){
+  timeSer <- getTimeSeries(symb)
+  #generate the kernel from here for now:
+  #http://dev.theomader.com/gaussian-kernel-calculator/
+  kern = c(0,0.000001,0.000002,0.000005,0.000012,0.000027,0.00006,0.000125,0.000251,0.000484,0.000898,0.001601,0.002743,0.004514,0.00714,0.010852,0.015849,0.022242,0.029993,0.038866,0.048394,0.057904,0.066574,0.073551,0.078084,0.079656,0.078084,0.073551,0.066574,0.057904,0.048394,0.038866,0.029993,0.022242,0.015849,0.010852,0.00714,0.004514,0.002743,0.001601,0.000898,0.000484,0.000251,0.000125,0.00006,0.000027,0.000012,0.000005,0.000002,0.000001,0)
+  wTrans = convolve(timeSer[["close"]], kern, type = "filter")
+  plot(wTrans, pch = ".")
+}
 
 #example return calculation
 #iYear <- timeSeries[250, "year_"]
@@ -75,3 +131,4 @@ run <- function(symb){
 
 #getAnnualizedReturn(iDay, iMon, iYear, iVal, fDay, fMon, fYear, fVal)
 
+ 
