1f2245a5b1c285222c8f73d5d63c8fab03cd9867
ismith1024 <ismith1024@gmail.com>
2019-02-16 21:46:33 -0500

    column headers for full advfn quarterly reports

--- soup_scraper.py
+++ soup_scraper.py
@@ -1,13 +1,62 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+
+
+#database libraries
 import sqlite3
+from sqlite3 import Error
+import sqlalchemy
+from sqlalchemy import create_engine
 
-sqlite_db = 'hi.db'
-database = sqlite3.connect(sqlite_db)
-curs = database.cursor()
 
-tickers = curs.get_all('SELECT * FROM TICKERS')
+'''
+engine = create_engine('sqlite:////home/ian/Data/advfn.db')
+df = pd.read_sql_table('tsx_companies', engine)
+'''
+
+#print(df)
+#tickers = curs.get_all('SELECT * FROM TICKERS')
+
+################### Get all column headers
+## Utility function to find all items for all quarterly reports
+def get_column_headers():
+
+    sqlite_db = '/home/ian/Data/advfn.db'
+    database = sqlite3.connect(sqlite_db)
+    curs = database.cursor()
+
+    curs.execute("select company_ticker from tsx_companies;")
+    results = curs.fetchall()
+    
+    tickers = []
+
+    for res in results:
+        tickers.append(res[0])
+
+    column_headers = set()
+    #for index, row in df.head(2).iterrows():
+    
+    for ticker in tickers:
+        print("Check: " + ticker)#row['company_ticker'])
+        url = 'https://ca.advfn.com/stock-market/TSX/' + ticker + '/financials?btn=istart_date&istart_date=1&mode=quarterly_reports'
+
+        resp = requests.get(url)
+        page = resp.text 
+        soup = BeautifulSoup(page, 'html.parser')
+
+        row_heads = soup.find_all('td', attrs={'class': 's', 'align' : 'left'})
+        
+        for tag in row_heads:
+            col_header = str(tag.text.strip())            
+            column_headers.add(col_header)
+
+    for header in column_headers:
+        sql = '''INSERT INTO column_headers(header) VALUES(?)'''
+        curs.execute(sql, (header,))
+    
+    database.commit()
+
 
 ################### Get present-day financials
 
@@ -47,7 +96,14 @@ def get_current_financials(url):
     return ret
 
 ####################### Get a quarterly report
+## TODO: need to count the cells with align:right
+## PageIndex: starts at 1 (the earliest quarterly report) and increases
+##            database intializes last_report_index to 0
+
 url_to_test2 = 'https://ca.advfn.com/stock-market/TSX/BNS/financials?btn=istart_date&istart_date=69&mode=quarterly_reports'
+
+url_to_test3 = 'https://ca.advfn.com/stock-market/TSX/BNS/financials?btn=istart_date&istart_date=92&mode=quarterly_reports'
+
 
 def get_quarter_report(url):
 
@@ -58,21 +114,29 @@ def get_quarter_report(url):
     row_heads = soup.find_all('td', attrs={'class': 's', 'align' : 'left'})
     row_data = soup.find_all('td', attrs={'class': 's', 'align' : 'right'})
 
+    #print('Row headers: ' + str(len(row_heads)))
+    #print('Row data: ' + str(len(row_data)))
+
+    num_cols = int(len(row_data) / len(row_heads))
+    print('Found ' + str(num_cols) + ' columns')
+    
     for index, val in enumerate(row_heads):
         print_str = str(val.text.strip()) + ' : '
-        for index2 in range(5):
-            print_str += ' -- ' + str(row_data[5*index + index2].text.strip())
+        for index2 in range(num_cols):
+            print_str += ' -- ' + str(row_data[num_cols*index + index2].text.strip())
 
         print(print_str)
 
     ret = print_str
-
+    
     return ret
 
-print("   SCRAPE CURRENT FINANCIALS")
-get_current_financials(url_to_get)
-print("   SCRAPE A QUARTER")
-get_quarter_report(url_to_test2)
+get_column_headers()
+
+#print("   SCRAPE CURRENT FINANCIALS")
+#get_current_financials(url_to_get)
+#print("   SCRAPE A QUARTER")
+#get_quarter_report(url_to_test2)
 
 
 
